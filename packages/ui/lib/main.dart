5486efefb7fe461df320698a8f445a8848956faa
xaatw0 <xaatw0@gmail.com>
2024-12-23 14:10:01 -0800

    buildPresenterを受け取れるように修正

--- packages/ui/lib/main.dart
+++ packages/ui/lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:mvp_flutter/mvp_flutter.dart';
 import 'package:ui/pages/search_repositories/search_repositories_page.dart';
 import 'l10n-gen/l10n.dart';
+import 'pages/search_repositories/search_repositories_presenter_interface.dart';
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({
+    super.key,
+    this.buildPresenter,
+  });
 
+  final SearchRepositoriesPresenterInterface Function(BaseView view)?
+  buildPresenter;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       localizationsDelegates: L10n.localizationsDelegates,
       supportedLocales: L10n.supportedLocales,
-      home: SearchRepositoriesPage(),
+      home: SearchRepositoriesPage(
+        buildPresenter: buildPresenter,
+      ),
     );
   }
 }
